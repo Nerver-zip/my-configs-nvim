a8031e49b144913aefa7edfd27de42a7c400846d
Nerver-zip <marceloaugusto12161@gmail.com>
2026-04-26 01:20:22 -0300

    updated theme

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -5,26 +5,26 @@
 local M = {}
 
 M.base46 = {
-    theme = "dark_horizon",
-    hl_override = {
+    theme = "catppuccin",
+    --hl_override = {
         -- Fundo geral
-        Normal = { bg = "#000000" },
-        NormalNC = { bg = "#000000" },
+    --    Normal = { bg = "#000000" },
+    --    NormalNC = { bg = "#000000" },
 
         -- Barra lateral (NvimTree)
-        NvimTreeNormal = { bg = "#000000" },
-        NvimTreeNormalNC = { bg = "#000000" },
+    --    NvimTreeNormal = { bg = "#000000" },
+    --    NvimTreeNormalNC = { bg = "#000000" },
 
         -- Divisórias e linhas de status
-        WinSeparator = { fg = "#000000", bg = "#000000" },
-        VertSplit = { fg = "#000000", bg = "#000000" },
-        StatusLine = { bg = "#000000" },
-        StatusLineNC = { bg = "#000000" },
+    --    WinSeparator = { fg = "#000000", bg = "#000000" },
+    --    VertSplit = { fg = "#000000", bg = "#000000" },
+    --    StatusLine = { bg = "#000000" },
+    --    StatusLineNC = { bg = "#000000" },
 
         -- Telescope (se você usar)
-        TelescopeNormal = { bg = "#000000" },
-        TelescopeBorder = { bg = "#000000", fg = "#000000" },
-    },
+    --    TelescopeNormal = { bg = "#000000" },
+    --    TelescopeBorder = { bg = "#000000", fg = "#000000" },
+    --},
 
 	-- hl_override = {
 	-- 	Comment = { italic = true },
